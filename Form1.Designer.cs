043df8280ae4ecda414ccaf8ebe49beabc0443db
Lyvann <lyvann241@gmail.com>
2026-04-19 22:54:32 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace CalculateApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnNum1 = new System.Windows.Forms.Button();
+            this.btnNum2 = new System.Windows.Forms.Button();
+            this.btnNum3 = new System.Windows.Forms.Button();
+            this.btnNum4 = new System.Windows.Forms.Button();
+            this.btnNum5 = new System.Windows.Forms.Button();
+            this.btnNum6 = new System.Windows.Forms.Button();
+            this.btnNum7 = new System.Windows.Forms.Button();
+            this.btnNum8 = new System.Windows.Forms.Button();
+            this.btnNum9 = new System.Windows.Forms.Button();
+            this.btnNum0 = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnMutiply = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(17, 0);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(361, 76);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnNum1
+            // 
+            this.btnNum1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnNum1.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNum1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnNum1.Location = new System.Drawing.Point(17, 82);
+            this.btnNum1.Name = "btnNum1";
+            this.btnNum1.Size = new System.Drawing.Size(121, 65);
+            this.btnNum1.TabIndex = 1;
+            this.btnNum1.Text = "1";
+            this.btnNum1.UseVisualStyleBackColor = false;
+            this.btnNum1.Click += new System.EventHandler(this.btnNum1_Click);
+            // 
+            // btnNum2
+            // 
+            this.btnNum2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnNum2.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNum2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnNum2.Location = new System.Drawing.Point(137, 82);
+            this.btnNum2.Name = "btnNum2";
+            this.btnNum2.Size = new System.Drawing.Size(121, 65);
+            this.btnNum2.TabIndex = 2;
+            this.btnNum2.Text = "2";
+            this.btnNum2.UseVisualStyleBackColor = false;
+            this.btnNum2.Click += new System.EventHandler(this.btnNum2_Click);
+            // 
+            // btnNum3
+            // 
+            this.btnNum3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnNum3.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNum3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnNum3.Location = new System.Drawing.Point(257, 82);
+            this.btnNum3.Name = "btnNum3";
+            this.btnNum3.Size = new System.Drawing.Size(121, 65);
+            this.btnNum3.TabIndex = 3;
+            this.btnNum3.Text = "3";
+            this.btnNum3.UseVisualStyleBackColor = false;
+            this.btnNum3.Click += new System.EventHandler(this.btnNum3_Click);
+            // 
+            // btnNum4
+            // 
+            this.btnNum4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnNum4.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNum4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnNum4.Location = new System.Drawing.Point(17, 149);
+            this.btnNum4.Name = "btnNum4";
+            this.btnNum4.Size = new System.Drawing.Size(121, 65);
+            this.btnNum4.TabIndex = 4;
+            this.btnNum4.Text = "4";
+            this.btnNum4.UseVisualStyleBackColor = false;
+            this.btnNum4.Click += new System.EventHandler(this.btnNum4_Click);
+            // 
+            // btnNum5
+            // 
+            this.btnNum5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnNum5.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNum5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnNum5.Location = new System.Drawing.Point(137, 149);
+            this.btnNum5.Name = "btnNum5";
+            this.btnNum5.Size = new System.Drawing.Size(121, 65);
+            this.btnNum5.TabIndex = 5;
+            this.btnNum5.Text = "5";
+            this.btnNum5.UseVisualStyleBackColor = false;
+            this.btnNum5.Click += new System.EventHandler(this.btnNum5_Click);
+            // 
+            // btnNum6
+            // 
+            this.btnNum6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnNum6.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNum6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnNum6.Location = new System.Drawing.Point(257, 149);
+            this.btnNum6.Name = "btnNum6";
+            this.btnNum6.Size = new System.Drawing.Size(121, 65);
+            this.btnNum6.TabIndex = 6;
+            this.btnNum6.Text = "6";
+            this.btnNum6.UseVisualStyleBackColor = false;
+            this.btnNum6.Click += new System.EventHandler(this.btnNum6_Click);
+            // 
+            // btnNum7
+            // 
+            this.btnNum7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnNum7.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNum7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnNum7.Location = new System.Drawing.Point(17, 215);
+            this.btnNum7.Name = "btnNum7";
+            this.btnNum7.Size = new System.Drawing.Size(121, 65);
+            this.btnNum7.TabIndex = 7;
+            this.btnNum7.Text = "7";
+            this.btnNum7.UseVisualStyleBackColor = false;
+            this.btnNum7.Click += new System.EventHandler(this.btnNum7_Click);
+            // 
+            // btnNum8
+            // 
+            this.btnNum8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnNum8.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNum8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnNum8.Location = new System.Drawing.Point(137, 215);
+            this.btnNum8.Name = "btnNum8";
+            this.btnNum8.Size = new System.Drawing.Size(121, 65);
+            this.btnNum8.TabIndex = 8;
+            this.btnNum8.Text = "8";
+            this.btnNum8.UseVisualStyleBackColor = false;
+            this.btnNum8.Click += new System.EventHandler(this.btnNum8_Click);
+            // 
+            // btnNum9
+            // 
+            this.btnNum9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnNum9.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNum9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnNum9.Location = new System.Drawing.Point(257, 215);
+            this.btnNum9.Name = "btnNum9";
+            this.btnNum9.Size = new System.Drawing.Size(121, 65);
+            this.btnNum9.TabIndex = 9;
+            this.btnNum9.Text = "9";
+            this.btnNum9.UseVisualStyleBackColor = false;
+            this.btnNum9.Click += new System.EventHandler(this.btnNum9_Click);
+            // 
+            // btnNum0
+            // 
+            this.btnNum0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnNum0.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNum0.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnNum0.Location = new System.Drawing.Point(137, 282);
+            this.btnNum0.Name = "btnNum0";
+            this.btnNum0.Size = new System.Drawing.Size(121, 65);
+            this.btnNum0.TabIndex = 10;
+            this.btnNum0.Text = "0";
+            this.btnNum0.UseVisualStyleBackColor = false;
+            this.btnNum0.Click += new System.EventHandler(this.btnNum0_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnAdd.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnAdd.Location = new System.Drawing.Point(17, 282);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(121, 65);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnDivide.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivide.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnDivide.Location = new System.Drawing.Point(257, 282);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(121, 65);
+            this.btnDivide.TabIndex = 12;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = false;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnEqual.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEqual.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnEqual.Location = new System.Drawing.Point(258, 346);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(121, 65);
+            this.btnEqual.TabIndex = 15;
+            this.btnEqual.Text = "=";
+            this.btnEqual.UseVisualStyleBackColor = false;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnMinus.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinus.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnMinus.Location = new System.Drawing.Point(18, 346);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(121, 65);
+            this.btnMinus.TabIndex = 14;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = false;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnMutiply
+            // 
+            this.btnMutiply.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnMutiply.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMutiply.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnMutiply.Location = new System.Drawing.Point(138, 346);
+            this.btnMutiply.Name = "btnMutiply";
+            this.btnMutiply.Size = new System.Drawing.Size(121, 65);
+            this.btnMutiply.TabIndex = 13;
+            this.btnMutiply.Text = "*";
+            this.btnMutiply.UseVisualStyleBackColor = false;
+            this.btnMutiply.Click += new System.EventHandler(this.btnMutiply_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnClear.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnClear.Location = new System.Drawing.Point(20, 413);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(358, 65);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 485);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnMutiply);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnNum0);
+            this.Controls.Add(this.btnNum9);
+            this.Controls.Add(this.btnNum8);
+            this.Controls.Add(this.btnNum7);
+            this.Controls.Add(this.btnNum6);
+            this.Controls.Add(this.btnNum5);
+            this.Controls.Add(this.btnNum4);
+            this.Controls.Add(this.btnNum3);
+            this.Controls.Add(this.btnNum2);
+            this.Controls.Add(this.btnNum1);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnNum1;
+        private System.Windows.Forms.Button btnNum2;
+        private System.Windows.Forms.Button btnNum3;
+        private System.Windows.Forms.Button btnNum4;
+        private System.Windows.Forms.Button btnNum5;
+        private System.Windows.Forms.Button btnNum6;
+        private System.Windows.Forms.Button btnNum7;
+        private System.Windows.Forms.Button btnNum8;
+        private System.Windows.Forms.Button btnNum9;
+        private System.Windows.Forms.Button btnNum0;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMutiply;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateApp
+{
+    public partial class Form1 : Form
+    {
+        //Declear variable
+
+        string Input = "";
+        double Results = 0;
+        string Operatoin = "";
+        bool OperationChoose = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //Number and Digit button
+        private void btnNum1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Input += btn.Text;
+            txtInput.Text = Input;
+        }
+
+        private void btnNum2_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Input += btn.Text;
+            txtInput.Text = Input;
+        }
+
+        private void btnNum3_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Input += btn.Text;
+            txtInput.Text = Input;
+        }
+
+        private void btnNum4_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Input += btn.Text;
+            txtInput.Text = Input;
+        }
+
+        private void btnNum5_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Input += btn.Text;
+            txtInput.Text = Input;
+        }
+
+        private void btnNum6_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Input += btn.Text;
+            txtInput.Text = Input;
+        }
+
+        private void btnNum7_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Input += btn.Text;
+            txtInput.Text = Input;
+        }
+
+        private void btnNum8_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Input += btn.Text;
+            txtInput.Text = Input;
+        }
+
+        private void btnNum9_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Input += btn.Text;
+            txtInput.Text = Input;
+        }
+
+        private void btnNum0_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Input += btn.Text;
+            txtInput.Text = Input;
+        }
+
+        //Make a method for operation in every button
+        private void Evaluate()
+        {
+           if(OperationChoose)
+            {
+                double SecondNum = double.Parse(Input);
+
+                switch (Operatoin)
+                {
+                    case "+":
+                        Results += SecondNum;
+                        break;
+                    case "-":
+                        Results -= SecondNum;
+                        break;
+                    case "*":
+                        Results *= SecondNum;
+                        break;
+                    case "/":
+                        if (SecondNum != 0)
+                        {
+                            Results /= SecondNum;
+                        }
+                        else
+                        {
+                            txtInput.Text = "Error!";
+                        }
+                        break;
+                }
+                txtInput.Text = Results.ToString();
+                Input = "";
+                OperationChoose = false;
+           }
+
+        }
+
+        //Button to calculate the number in the calculate
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Input = "";
+            Operatoin = "";
+            Results = 0;
+            OperationChoose = false;
+            txtInput.Text = "";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (OperationChoose)
+            {
+                Evaluate();
+            }
+            Button btn = (Button)sender;
+            Operatoin = btn.Text; //This is mean Operation = what we are click the button for
+            Results = double.Parse(Input); //We declear the result as double so we need to convert the result as string 
+            Input = "";
+            OperationChoose = true;
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            if (OperationChoose)
+            {
+                Evaluate();
+            }
+            Button btn = (Button)sender;
+            Operatoin = btn.Text; //This is mean Operation = what we are click the button for
+            Results = double.Parse(Input); //We declear the result as double so we need to convert the result as string 
+            Input = "";
+            OperationChoose = true;
+        }
+
+        private void btnMutiply_Click(object sender, EventArgs e)
+        {
+            if (OperationChoose)
+            {
+                Evaluate();
+            }
+            Button btn = (Button)sender;
+            Operatoin = btn.Text; //This is mean Operation = what we are click the button for
+            Results = double.Parse(Input); //We declear the result as double so we need to convert the result as string 
+            Input = "";
+            OperationChoose = true;
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            if (OperationChoose)
+            {
+                Evaluate();
+            }
+            Button btn = (Button)sender;
+            Operatoin = btn.Text; //This is mean Operation = what we are click the button for
+            Results = double.Parse(Input); //We declear the result as double so we need to convert the result as string 
+            Input = "";
+            OperationChoose = true;
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            Evaluate();
+        }
+    }
+}
